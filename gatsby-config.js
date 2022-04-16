9af01101364a0015bdac19aa2b6ce09d9182e51f
khalilahmad3212 <arzishjani1613@gmail.com>
2022-04-16 12:07:22 +0500

    First Commit

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Khalil Ahmad`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function IndexPage() {
+  return (
+    <div>
+      <h1>Let's have fun!!!!</h1>
+      <p>This is noice</p>
+    </div>
+  )
+}
+
+export default IndexPage
